0c2ea577e994e6c936cb1e080832b1fcec3611a2
Ahmed Gamal <gis.csharp@gmail.com>
2020-12-26 01:57:37 +0300

    Add project files.

---
+++ EmployeeManagments/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EmployeeManagments.Models;
+
+namespace EmployeeManagments.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IEmployeeRepository _employeeReposiory;
+
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeReposiory = employeeRepository;
+        }
+        public string Index()
+        {
+            return _employeeReposiory.GetEmployee(1).Name;
+        }
+    }
+}
---
+++ EmployeeManagments/Models/MockEmployeeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeManagments.Models
+{
+    public class MockEmployeeRepository : IEmployeeRepository
+    {
+        private List<Employee> _employee;
+
+        public MockEmployeeRepository()
+        {
+            _employee = new List<Employee>()
+            {
+                new Employee() {Id = 1, Name = "Ahmed", Department = "IT", Email = "agmghazi@hotmail.com"},
+                new Employee() {Id = 2, Name = "ayman", Department = "IT", Email = "agmghazi2@hotmail.com"},
+                new Employee() {Id = 3, Name = "eslam", Department = "HR", Email = "agmghazi3@hotmail.com"},
+            };
+        }
+        public Employee GetEmployee(int id)
+        {
+            return this._employee.FirstOrDefault(x => x.Id == id);
+        }
+    }
+}
