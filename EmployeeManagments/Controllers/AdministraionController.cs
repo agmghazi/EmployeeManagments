4b97b1c4e82a3498ad9937af0416a2c13f36f9ae
Ahmed Gamal <gis.csharp@gmail.com>
2020-12-31 21:24:21 +0300

    add role and list of roles

---
+++ EmployeeManagments/Controllers/AdministraionController.cs
@@ -0,0 +1,53 @@
+﻿using EmployeeManagments.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace EmployeeManagments.Controllers
+{
+    public class AdministraionController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public AdministraionController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public IActionResult CreateRole()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateRole(CreateRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityRole identityRole = new IdentityRole
+                {
+                    Name = model.RoleName
+                };
+                IdentityResult result = await _roleManager.CreateAsync(identityRole);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("ListRoles", "Administraion");
+                }
+                foreach (IdentityError error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult ListRoles()
+        {
+            var roles = _roleManager.Roles;
+            return View(roles);
+        }
+    }
+}
